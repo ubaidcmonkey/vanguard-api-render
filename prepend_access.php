ded4b02f28786b3677dbce36af5a7d87cb923ff2
ubaidcmonkey <140199630+ubaidcmonkey@users.noreply.github.com>
2026-07-14 15:37:04 +0100

    Update prepend_access.php

--- prepend_access.php
+++ prepend_access.php
@@ -2,6 +2,4 @@
 require_once __DIR__ . '/access_control.php';
 
 $requestPath = parse_url($_SERVER['REQUEST_URI'] ?? '', PHP_URL_PATH);
-if ($requestPath !== '/health.php') {
-    reject_unlisted_ip();
-}
+reject_unlisted_ip();
